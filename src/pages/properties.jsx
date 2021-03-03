a58c5e60aa64260e8a741ee5737a0bc7f83d656d
Mustapha Oyenuga <musty1k@yahoo.com>
2021-03-03 13:38:29 +0100

    property updates

---
+++ src/pages/properties.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import "./properties.css";
+
+import pic1 from "../assets/pic1.jpg";
+import pic2 from "../assets/pic2.jpg";
+import pic3 from "../assets/pic3.jpg";
+import slider from "../assets/slider-1.jpg";
+
+
+const estate = () => {
+  return (
+    <Carousel autoPlay className="carosel z-0 " dynamicHeight={true}  >
+      <div>
+        <img alt="" src={pic1} />
+        <p className="legend">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore,
+          fugiat.
+        </p>
+      </div>
+      <div>
+        <img alt="" src={pic2} />
+        <p className="legend">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore,
+          fugiat.
+        </p>
+      </div>
+      <div>
+        <img alt="" src={pic3} />
+        <p className="legend">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore,
+          fugiat.
+        </p>
+      </div>
+      <div>
+        <img alt="" src={slider} />
+        <p className="legend">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore,
+          fugiat.
+        </p>
+      </div>
+    
+    </Carousel>
+  );
+};
+
+export default estate;
